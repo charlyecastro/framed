81ee02d723e7696ba4c5ae84b7369af08e0e1fb7
charlyecastro <castroc@uw.edu>
2020-03-02 14:37:49 -0800

    added starter UI

--- app/src/App.js
+++ app/src/App.js
@@ -13,54 +13,67 @@ function App() {
 
   }, []);
 
-  const fetchImgList = ( () => {
+  const fetchImgList = (() => {
     Axios.get("https://api.unsplash.com/photos/")
-    .then(res => {
-      console.log(res)
-      return res.data
-    })
-    .catch(err => {
-      console.log(err);
-    })
-    .then(data => {
-      setimgList(data)
-      console.log(data)
-    })
+      .then(res => {
+        console.log(res)
+        return res.data
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .then(data => {
+        setimgList(data)
+        console.log(data)
+      })
   });
 
-  const fetchImgBanner = ( () => {
+  const fetchImgBanner = (() => {
     Axios.get("https://api.unsplash.com/photos/random")
-    .then(res => {
-      console.log(res)
-      return res.data
-    })
-    .catch(err => {
-      console.log(err);
-    })
-    .then(data => {
-      setImgBanner(data)
-      console.log(data)
-    })
+      .then(res => {
+        console.log(res)
+        return res.data
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .then(data => {
+        setImgBanner(data)
+        console.log(data)
+      })
   });
 
   return (
     <div className="App container">
       <header className="App-header">
-        <div className = "banner">
-      {!imgBanner ? 
-        "loading..." 
-        : <img src = {imgBanner.urls.regular} alt ={imgBanner.alt_description}/>
-      }
-      </div>
-        <div className = "gallery">
-        {!imgList ? 
-        "loading..." 
-        :
-        imgList.map(img => { 
-          return <img key = {img.id} src = {img.urls.small} alt = {img.alt_description}/> 
-         }) }
-         </div>
-    
+        <div className="banner">
+          {!imgBanner ?
+            "loading..."
+            : <img src={imgBanner.urls.regular} alt={imgBanner.alt_description} />
+          }
+          <div className="overlay">
+            <div className = "content">
+            <h1>The coolest pics framed by the coolest people</h1>
+            <input type="text" id="img" name="img" placeholder="Search for some pics"/>
+            <div className="button-row">
+              <button>New</button>
+              <button>Popular</button>
+              <button>Random</button>
+            </div>
+            </div>
+            
+           
+          </div>
+        </div>
+        <div className="gallery">
+          {!imgList ?
+            "loading..."
+            :
+            imgList.map(img => {
+              return <img key={img.id} src={img.urls.small} alt={img.alt_description} />
+            })}
+        </div>
+
       </header>
     </div>
   );
