9235be8cdbe913434d5c35f2040443cf5b3c0922
charlyecastro <castroc@uw.edu>
2020-03-02 14:08:39 -0800

    fetched all images and added some styling

--- app/src/App.js
+++ app/src/App.js
@@ -3,35 +3,63 @@ import './styles/App.css';
 import Axios from 'axios';
 
 function App() {
-  const [imgData, setImgData] = useState([]);
+  const [imgBanner, setImgBanner] = useState('');
+  const [imgList, setimgList] = useState([]);
 
   useEffect(() => {
     Axios.defaults.headers.common['Authorization'] = `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`
-    Axios.get("https://api.unsplash.com/photos/")
-      .then(res => {
-        console.log(res)
-        return res.data
-      })
-      .catch(err => {
-        console.log(err);
-      })
-      .then(data => {
-        setImgData(data)
-        console.log(data)
-      })
+    fetchImgList()
+    fetchImgBanner()
 
   }, []);
 
+  const fetchImgList = ( () => {
+    Axios.get("https://api.unsplash.com/photos/")
+    .then(res => {
+      console.log(res)
+      return res.data
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .then(data => {
+      setimgList(data)
+      console.log(data)
+    })
+  });
+
+  const fetchImgBanner = ( () => {
+    Axios.get("https://api.unsplash.com/photos/random")
+    .then(res => {
+      console.log(res)
+      return res.data
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .then(data => {
+      setImgBanner(data)
+      console.log(data)
+    })
+  });
+
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-        {!imgData ? 
+        <div className = "banner">
+      {!imgBanner ? 
+        "loading..." 
+        : <img src = {imgBanner.urls.regular} alt ={imgBanner.alt_description}/>
+      }
+      </div>
+        <div className = "gallery">
+        {!imgList ? 
         "loading..." 
         :
-        imgData.map(img => { 
-          return <img id = {img.id} src = {img.urls.small} /> 
+        imgList.map(img => { 
+          return <img key = {img.id} src = {img.urls.small} alt = {img.alt_description}/> 
          }) }
-         
+         </div>
     
       </header>
     </div>
