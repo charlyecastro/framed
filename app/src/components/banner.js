78d7d5d4c0a2be7fb4f2c841e74a21427a3d5eab
charlyecastro <castroc@uw.edu>
2020-06-29 18:14:33 -0700

    Updates

--- app/src/components/banner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const banner = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
---
+++ precache-manifest.c1891ec68aa70990cea92ee463bd8599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bc76ab401bdf8807ba31d2ce364449",
+    "url": "/framed/index.html"
+  },
+  {
+    "revision": "9a917aa2d9a3080d78d8",
+    "url": "/framed/static/css/main.d368b0d1.chunk.css"
+  },
+  {
+    "revision": "dae73b9a3eb6797277d6",
+    "url": "/framed/static/js/2.83358a42.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/framed/static/js/2.83358a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a917aa2d9a3080d78d8",
+    "url": "/framed/static/js/main.aa2c5cb2.chunk.js"
+  },
+  {
+    "revision": "e0c32ca734ba2e089dd0",
+    "url": "/framed/static/js/runtime-main.3df2f607.js"
+  }
+]);
